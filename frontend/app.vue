5800f04f0cdc46247df4778b4075062d4cf4d6b2
Kentaro <kentaro@beah.jp>
2023-04-29 19:04:37 +0900

    .

---
+++ frontend/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <div class="flex items-center justify-between flex-wrap bg-green-600 p-6 mb-3">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+            <span class="font-semibold text-xl tracking-tight">Google Chat Sender</span>
+        </div>
+      </div>
+      <div class="container mx-auto">
+          <create-message></create-message>
+      </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
