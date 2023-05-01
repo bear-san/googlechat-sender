ff032c9e67128235617206743a50369c17f51252
Kentaro <kentaro@beah.jp>
2023-05-01 12:58:08 +0900

    add: 予約された投稿の処理

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
 	"github.com/bear-san/googlechat-sender/backend/cmd"
+	"github.com/bear-san/googlechat-sender/backend/ent/postschedule"
+	auth2 "github.com/bear-san/googlechat-sender/backend/internal/auth"
+	"github.com/bear-san/googlechat-sender/backend/internal/db"
+	"github.com/bear-san/googlechat-sender/backend/pkg/auth"
+	"github.com/bear-san/googlechat-sender/backend/pkg/chat"
 	"os"
+	"time"
 )
 
 func main() {
@@ -11,5 +18,31 @@ func main() {
 		cmd.Start()
 	} else if args[1] == "scheduler" {
 		// 予約投稿の処理用
+		ctx := context.Background()
+		t := time.Now()
+
+		schedules, err := db.Client.PostSchedule.Query().
+			Where(postschedule.IsSentEQ(false)).
+			All(ctx)
+
+		if err != nil {
+			panic(err)
+		}
+
+		for _, schedule := range schedules {
+			if schedule.SendAt.Unix() < t.Unix() {
+				u, err := db.Client.SystemUser.Get(ctx, schedule.UID)
+				if err != nil {
+					continue
+				}
+
+				token, err := auth.GetGoogleCredential(ctx, u, auth2.GetOAuthClientInfo())
+				if err != nil {
+					continue
+				}
+
+				_, err = chat.Publish(ctx, token, schedule)
+			}
+		}
 	}
 }
